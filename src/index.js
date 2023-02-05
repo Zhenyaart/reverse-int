3bdbf91769a4a00c3a1a743d1d649a75885c4fdc
Zhenyaart <logitech22139@mail.ru>
2023-02-05 22:26:40 +0300

    reverse-init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const { eq } = require("semver");
+
 module.exports = function reverse (n) {
-  
+    
+    const a = String(n).replace(/-/g,'');
+
+    const b = a.split('').reverse().join('');
+
+    return b;
+
 }
+
